dc7aae179470c4ad536e144785eebcfe4f996f99
alexthai <axt.421@gmail.com>
2018-04-19 22:04:27 -0400

    updated request mapping to /helloworld8081

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-    @RequestMapping("/")
+    @RequestMapping("/helloworld8081")
     public String home() {
         return "Hello Docker World 8081";
     }
